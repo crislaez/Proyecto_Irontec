e909e7f7bfc18cd16464ad5169d4171d2b313a4d
CrisLaez <you@example.comcrislaez30@gmail.com>
2020-06-26 13:49:21 +0200

    agregado buscar por repositorio

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -11,10 +11,12 @@ import {enviroment} from '../../../../enviroment.prod'
 })
 export class InicioComponent implements OnInit,DoCheck {
 
-  public nombre:String;
+  public nombre:String; //variable para buscar todos los repositorios por usuario
   public mostrarDatos:Boolean; //variable para mostra los divs escondidos
-  // public datosUsuarios: Array<Object>;
-  public datosUsuarios:any[];
+  public mostrarTodosRepos:Boolean;//variable qeu cargara el div que SI tiene el ngfor
+  public mostrarUnRepo:Boolean; //variable que cargara el div que No tiene ngFor
+  public nombreRepo:String; //varaible del formulario para buscar pro repositorio
+  public datosUsuarios:any;
   public imagenUsuario:String; //imagen del usuario del git
   public nombreUsuario:String //nombre del usuario git
   public contadorPagina:number; //para el paginado
@@ -26,6 +28,7 @@ export class InicioComponent implements OnInit,DoCheck {
 
   ngOnInit(): void {
     this.mostrarDatos = false;
+    this.mostrarUnRepo = false;
     this.contadorPagina = 1;
   };
 
@@ -33,10 +36,9 @@ export class InicioComponent implements OnInit,DoCheck {
 
   };
   
-  //funcion para el formulñario
+  //buscar repositorios por usuarios
   handleSubmit(){
-    event.preventDefault();
-    
+    event.preventDefault();    
     //si la variable esta vacia, retorna
     if(!this.nombre){
       alert('Rellene el campo de texto');
@@ -50,7 +52,6 @@ export class InicioComponent implements OnInit,DoCheck {
         alert('No se ha encontraod usuario');
       }else{
         this.mostrarDatos = true; 
-        console.log(response)
         this.imagenUsuario = response.avatar_url
         this.nombreUsuario = response.login
         this.ruta = response.repos_url;
@@ -62,6 +63,34 @@ export class InicioComponent implements OnInit,DoCheck {
 
     //vacaimos el input
     this.nombre = ''
+  };
+
+  //buscar por repositorio
+  handleSubmitBuscarRepo(){
+    event.preventDefault();  
+    if(!this.nombreRepo){
+      alert('Rellene el nombre del repo')
+    }else{
+      // enviroment.rutarepos+this.usuarioGit+'/'+this.repoGit
+      getGitData(enviroment.rutarepos+this.nombreUsuario+'/'+this.nombreRepo)
+      .then((response:any) => {
+        //si la respuesta tiene mensaje signuifica que no hay datos
+        if(!response.message){
+          //hacemos que el div que tiene el ngFor sea false para que no se cargue
+          this.mostrarTodosRepos = false;
+          //y el div que no tiene, se carge
+          this.mostrarUnRepo = true;
+          this.datosUsuarios = response
+          console.log('aqui')
+          console.log(this.datosUsuarios.name)         
+        }else{
+          alert('No existe ese repositorio')
+        }        
+      })
+      .catch(err => console.log(err))
+    }
+    //limpiamos el campo de texto
+    this.nombreRepo = ''
   };
 
   //funcion para el boton atras
@@ -79,7 +108,7 @@ export class InicioComponent implements OnInit,DoCheck {
 
   //funcion para el boton siguiente
   handleCLickSiguientes(){
-    
+    //si la longitud del array es mebnos de 30 significa que no hay mas proeyctos, por lo tanto no podra dar siguiente    
     if(this.datosUsuarios.length < 30){
       alert('No hay mas repositorios')
     }else{
@@ -94,9 +123,11 @@ export class InicioComponent implements OnInit,DoCheck {
   funcionFetch(pagina){
     getGitData(this.ruta +`?page=${this.contadorPagina}&per_page=30`)
     .then((response:any) => {
-       this.datosUsuarios = [];
+      //hacemos que el div que tiene el ngFor sea false para que se cargue
+       this.mostrarTodosRepos = true;
+       //y el div que no lo tiene no se cargue
+       this.mostrarUnRepo = false;
        this.datosUsuarios = response
-      //  console.log(this.datosUsuarios)
     })
     .catch(err => console.log(err))
   }
